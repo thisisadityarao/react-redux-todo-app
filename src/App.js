46c66229e9778b6686b79fb345b7fd52ea487af8
Aditya Rao <dev.adityarao@gmail.com>
2020-01-15 06:24:26 +0530

    Create action, reducer, store folder for Redux. Apply basic styles to App component and header.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import styled from "styled-components/macro";
+
+const Wrapper = styled.div`
+  width: 100%;
+  max-width: 768px;
+  min-width: 360px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  width: 100%;
+  background: pink;
+
+  & h1 {
+    text-align: center;
+    margin: 0;
+    padding: 1rem 0.5rem;
+  }
+`;
 
 function App() {
   return (
-    <div>
-      <header>
+    <Wrapper>
+      <Header>
         <h1>React Redux To-Do App</h1>
-      </header>
-    </div>
+      </Header>
+    </Wrapper>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import "./index.css";
+import { createGlobalStyle } from "styled-components";
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell,              Noto Sans, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+                "Segoe UI Symbol", "Noto Color Emoji";
+  }
+
+  body {
+    height: 100%;
+    width: 100%;
+    background-color: lightgoldenrodyellow;
+  }
+`;
+
+
+ReactDOM.render(
+  <React.Fragment>
+    <GlobalStyle />
+    <App />
+  </React.Fragment>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
